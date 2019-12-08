ed4e56dd56e37a049192b66bf97d07301218a1b3
Naoki Oketani <okepy.naoki@gmail.com>
2019-12-09 08:36:22 +0900

    npm run format; npm run lint

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github';
+import * as github from '@actions/github'
 import stripAnsi from 'strip-ansi'
-import Octokit, { IssuesCreateResponse } from '@octokit/rest';
-import { spawnSync, SpawnSyncReturns } from 'child_process';
+import Octokit, {IssuesCreateResponse} from '@octokit/rest'
+import {spawnSync, SpawnSyncReturns} from 'child_process'
 
 async function run(): Promise<void> {
   try {
     const result: SpawnSyncReturns<string> = spawnSync('npm', ['audit'], {
-      encoding: 'utf-8',
-    });
+      encoding: 'utf-8'
+    })
 
     if (result.stderr && result.stderr.length > 0) {
       throw new Error(result.stderr)
@@ -22,20 +22,20 @@ async function run(): Promise<void> {
     }
 
     core.debug('open an issue')
-    const token: string = core.getInput('token', { required: true })
+    const token: string = core.getInput('token', {required: true})
     const client: Octokit = new github.GitHub(token)
 
     // remove control characters and create a code block
-    const issueBody: string = `\`\`\`\n${stripAnsi(result.stdout)}\n\`\`\``
+    const issueBody = `\`\`\`\n${stripAnsi(result.stdout)}\n\`\`\``
     const issueOptions = {
       title: 'npm audit found vulnerabilities',
-      body: issueBody,
+      body: issueBody
     }
     const {
-      data: issue,
+      data: issue
     }: Octokit.Response<IssuesCreateResponse> = await client.issues.create({
       ...github.context.repo,
-      ...issueOptions,
+      ...issueOptions
     })
     core.debug(`#${issue.number}`)
   } catch (error) {
