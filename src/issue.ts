9f485bd049bedd971f1c2c78472fc54e8322c852
Naoki Oketani <okepy.naoki@gmail.com>
2019-12-09 22:44:56 +0900

    Creates an issue even if inputs.issue_labels is not given

--- src/issue.ts
+++ src/issue.ts
@@ -2,16 +2,20 @@ import * as core from '@actions/core'
 import {IssueOption} from './interface'
 
 export function getIssueOption(body: string): IssueOption {
+  let assignees
+  let labels
+
+  if (core.getInput('issue_assignees')) {
+    assignees = core.getInput('issue_assignees').replace(/\s+/g, '').split(',')
+  }
+  if (core.getInput('issue_labels')) {
+    labels = core.getInput('issue_labels').replace(/\s+/g, '').split(',')
+  }
+
   return {
     title: core.getInput('issue_title'),
     body,
-    assignees: core
-      .getInput('issue_assignees')
-      .replace(/\s+/g, '')
-      .split(','),
-    labels: core
-      .getInput('issue_labels')
-      .replace(/\s+/g, '')
-      .split(',')
+    assignees,
+    labels
   }
 }
