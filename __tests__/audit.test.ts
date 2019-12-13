a0a6093710c7164a0de1099683bb8b696b5d3992
Naoki Oketani <okepy.naoki@gmail.com>
2019-12-13 16:09:10 +0900

    add unit test and cleanup (#28)
    * remove unnessary import
    
    * use mock for child_process.spawnSync()
    
    * document useful resources
    
    * use v1.1.0 for daily scan

--- __tests__/audit.test.ts
+++ __tests__/audit.test.ts
@@ -1,15 +1,57 @@
+import * as child_process from 'child_process'
+import * as fs from 'fs'
+import * as path from 'path'
+import {mocked} from 'ts-jest/utils'
 import {Audit} from '../src/audit'
+
+jest.mock('child_process')
 
 const audit = new Audit()
 
-describe('npm audit', () => {
+describe('run', () => {
+  beforeEach(() => {
+    mocked(child_process).spawnSync.mockClear()
+  })
+
   test('finds vulnerabilities', () => {
-    audit.status = 1
+    mocked(child_process).spawnSync.mockImplementation((): any => {
+      const stdout = fs.readFileSync(
+        path.join(__dirname, 'testdata/audit/error.txt')
+      )
+
+      return {
+        pid: 100,
+        output: [stdout],
+        stdout,
+        stderr: '',
+        status: 1,
+        signal: null,
+        error: null
+      }
+    })
+
+    audit.run()
     expect(audit.foundVulnerability()).toBeTruthy()
   })
 
   test('does not find vulnerabilities', () => {
-    audit.status = 0
+    mocked(child_process).spawnSync.mockImplementation((): any => {
+      const stdout = fs.readFileSync(
+        path.join(__dirname, 'testdata/audit/success.txt')
+      )
+
+      return {
+        pid: 100,
+        output: [stdout],
+        stdout,
+        stderr: '',
+        status: 0,
+        signal: null,
+        error: null
+      }
+    })
+
+    audit.run()
     expect(audit.foundVulnerability()).toBeFalsy()
   })
 })
